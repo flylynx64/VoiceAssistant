b6b9d40f9d2d18be2d1bc8979082b83ba6dfb73b
Zachary Wang <47799146+flylynx64@users.noreply.github.com>
2020-08-17 20:02:20 -0500

    Update Voice.py

--- Voice.py
+++ Voice.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import datetime
 import pickle
+import face_recognition
+import cv2
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -270,9 +272,98 @@ def getDate(text):
 		return currentDate + datetime.timedelta(diff)
 	return datetime.date(month = month, day = day, year = year) 
 
+def learnFaces():
+	cam = cv2.VideoCapture(0)
+	name = input('Enter your name: ') 
+	cv2.namedWindow("Learn Face")
+	img_counter = 0
+	while True:
+		ret, frame = cam.read()
+		if not ret:
+			break
+		cv2.imshow("Learn Face", frame)
+		k = cv2.waitKey(1)
+		if k == 27:
+			print("Escape hit, closing...")
+			break
+		elif k == 32:
+			img_name = "opencv_frame.jpg"
+			cv2.imwrite(img_name, frame)
+			person_image = face_recognition.load_image_file(img_name)
+			if len(face_recognition.face_encodings(person_image)) > 0:
+				print("Face Detected! Count " + str(img_counter))
+				person_face_encoding = face_recognition.face_encodings(person_image)[0]
+				try:
+					pickle_in = open("facesData.pickle","rb")
+					encoding_data = pickle.load(pickle_in)
+				except IOError:
+					encoding_data = {}
+				
+				if name in encoding_data:
+					encoding_data[name].append(person_face_encoding)
+				else:
+					encoding_data[name] = [person_face_encoding]
+				pickle_out = open("facesData.pickle","wb")
+				pickle.dump(encoding_data, pickle_out)
+				pickle_out.close()
+				img_counter += 1
+	cam.release()
+	cv2.destroyAllWindows()
+
+def recognize():
+	cam = cv2.VideoCapture(0)
+	try:
+		pickle_in = open("facesData.pickle","rb")
+		encoding_data = pickle.load(pickle_in)
+	except IOError:
+		encoding_data = {}
+	known_face_names = []
+	known_face_encodings = []
+	for key, value in encoding_data.items():
+		for val in value:
+			known_face_names.append(key)
+			known_face_encodings.append(val)
+	face_locations = []
+	face_encodings = []
+	face_names = []
+	process_this_frame = True
+	while True:
+	    ret, frame = cam.read()
+	    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+	    rgb_small_frame = small_frame[:, :, ::-1]
+	    if process_this_frame:
+	        face_locations = face_recognition.face_locations(rgb_small_frame)
+	        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+	        face_names = []
+	        for face_encoding in face_encodings:
+	            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+	            name = "Unknown"
+	            if True in matches:
+	                first_match_index = matches.index(True)
+	                name = known_face_names[first_match_index]
+	            face_names.append(name)
+	    process_this_frame = not process_this_frame
+	    for (top, right, bottom, left), name in zip(face_locations, face_names):
+	        top *= 4
+	        right *= 4
+	        bottom *= 4
+	        left *= 4
+	        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+	        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), -1)
+	        font = cv2.FONT_HERSHEY_DUPLEX
+	        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+	    cv2.imshow('Video', frame)
+	    k = cv2.waitKey(1)
+	    if k%256 == 27:
+	        print("Escape hit, closing...")
+	        break
+	cam.release()
+	cv2.destroyAllWindows()
+
 def main():
 	global count
 	count = 0
+	if()
 	while(True):
 		speak("Hello. Choose speech or text.")
 		text = "text" #getAudio().lower()
